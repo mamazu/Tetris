cd5c0efb4a021f7ab79fd775aa0aed0e24a47291
Sigma5000 <dumm.doof.ts.sn@gmail.com>
2017-04-15 14:25:07 +0200

    Steine angelegt
    FERTIG!!!!!!!!!!!!!!!!!!!!
    
    Hallo?
    Was jetzt?

---
+++ src/Stein.java
@@ -0,0 +1,12 @@
+public class Stein{
+
+	public Stein Stein;
+
+	public Stein(){
+		 Stein = new Stein((int) Math.random() * 7);
+	}
+
+	public Stein(int h){
+		Stein = new Stein(h);
+	}
+}
---
+++ src/SteinTyp.java
@@ -0,0 +1,28 @@
+public class SteinTyp extends Stein{
+
+	private final boolean[][] Stein_ = new boolean[4][4];
+
+	public SteinTyp(int x){
+		switch(x){
+			case 0:		for(int i=0; i<4;i++)	Stein_[i][0] = true; break;
+			case 1:		Stein_[0][0] = true;	for(int i=0; i<= 2; i++) Stein_[0][i] = true; break;
+			case 2:		for(int i=0; i<=2; i++) Stein_[1][i] = true; Stein_[0][2] = true;	break;
+			case 3:
+				for(int i=0; i<=1;i++){
+					Stein_[0][i] = true;
+					Stein_[1][i] = true;
+				}
+				break;
+			case 4:
+				for(int i=0; i<=1;i++){
+					Stein_[0][i+1] = true;
+					Stein_[1][i] = true;
+				}
+				break;
+			case 5:
+				for 
+			case 6:
+			default:	System.out.println("Fehler mit unbekanntem Fehlercode :D");
+		}
+	}
+}
