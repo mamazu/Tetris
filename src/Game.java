01c79568dd6da8d8b5ceb4a6bcebec38046d0bf7
mamazu <maxo.pech@gmail.com>
2017-04-16 16:14:04 +0200

    Removed testing bits and pieces

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,6 @@ public class Game{
 			System.out.print(g.board.toString());
 			System.out.println("Next stone is: " + g.next.toString());
 			System.out.println("Next pattern is: \n" + g.next.getPattern());
-			System.out.println("After rotating it is: \n" + g.next.rotate());
 			return;
 		}
 		// Game with GUI
