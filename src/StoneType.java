e460d3e10513890dced0e4443a86900ae714ba91
mamazu <maxo.pech@gmail.com>
2017-04-16 12:28:54 +0200

    Added stonetype array

--- src/StoneType.java
+++ src/StoneType.java
@@ -1,6 +1,15 @@
 public class StoneType extends Stone {
 
-	private final boolean[][] Stein_ = new boolean[4][4];
+enum StoneTypes{
+	// http://www.russlandjournal.de/unterhaltung/tetris/attachment/tetris-steine/
+	BAR, // dark blue
+	T, // light blue
+	SQUARE, //green
+	S, // orange
+	Z, // pink
+	L, // red
+	J; // yellow
+}
 
 	public StoneType(int x){
 		switch(x){
