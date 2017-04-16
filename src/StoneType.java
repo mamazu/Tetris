341bd2ec386aa214adae8fd4fa8e73ccc6b096de
mamazu <maxo.pech@gmail.com>
2017-04-16 12:33:12 +0200

    Reworked the stonetype class

--- src/StoneType.java
+++ src/StoneType.java
@@ -1,4 +1,4 @@
-public class StoneType extends Stone {
+import java.util.Random;
 
 enum StoneTypes{
 	// http://www.russlandjournal.de/unterhaltung/tetris/attachment/tetris-steine/
@@ -8,29 +8,64 @@ enum StoneTypes{
 	S, // orange
 	Z, // pink
 	L, // red
-	J; // yellow
+	J // yellow
 }
 
-	public StoneType(int x){
+class StoneType {
+
+	private static StoneTypes random() {
+		final StoneTypes[] values = StoneTypes.values();
+		int pick = new Random().nextInt(values.length);
+		return values[pick];
+	}
+
+	public static boolean[][] getPattern(){
+		return getPattern(random());
+	}
+
+	public static boolean[][] getPattern(StoneTypes x){
+		boolean[][] pattern = new boolean[4][4];
 		switch(x){
-			case 0:		for(int i=0; i<4;i++)	Stein_[i][0] = true; break;
-			case 1:		Stein_[0][0] = true;	for(int i=0; i<= 2; i++) Stein_[0][i] = true; break;
-			case 2:		for(int i=0; i<=2; i++) Stein_[1][i] = true; Stein_[0][2] = true;	break;
-			case 3:
-				for(int i=0; i<=1;i++){
-					Stein_[0][i] = true;
-					Stein_[1][i] = true;
+			case BAR:
+				for(int i=0; i<4;i++)
+					pattern[i][0] = true;
+				break;
+			case J:
+			case L:
+				for(int i=0; i < 3; i++)
+					pattern[0][i] = true;
+				if(x == StoneTypes.L){
+					pattern[1][2] = true;
+				}else {
+					pattern[1][0] = true;
 				}
 				break;
-			case 4:
+			case T:
+				for(int i=0; i<=2; i++)
+					pattern[1][i] = true;
+				pattern[0][2] = true;
+				break;
+			case SQUARE:
 				for(int i=0; i<=1;i++){
-					Stein_[0][i+1] = true;
-					Stein_[1][i] = true;
+					pattern[0][i] = true;
+					pattern[1][i] = true;
 				}
 				break;
-			case 5: break;
-			case 6:
-			default:	System.out.println("Fehler mit unbekanntem Fehlercode :D");
+			case S:
+				for(int i=0; i< 2;i++){
+					pattern[0][i+1] = true;
+					pattern[1][i] = true;
+				}
+				break;
+			case Z:
+				for(int i=0; i< 2;i++){
+					pattern[0][i] = true;
+					pattern[1][i+1] = true;
+				}
+				break;
+			default:
+				System.err.print("Invalid Stone type");
 		}
+		return pattern;
 	}
 }
