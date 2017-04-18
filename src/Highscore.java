10d290de5124f0d887f3ff341253a96f2d1d25f0
mamazu <maxo.pech@gmail.com>
2017-04-19 01:07:36 +0200

    Added score listing functionality

--- src/Highscore.java
+++ src/Highscore.java
@@ -1,17 +1,54 @@
+import Score.Score;
 import tools.FileHandler;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Highscore {
-    private FileHandler f;
+
+    public FileHandler f;
+    private LinkedList<Score> scores = new LinkedList<Score>();
 
     public Highscore(String name){
         f = new FileHandler(name);
         for(String line : f.getContent()){
+            if(line.startsWith("#")) continue;
+            String[] part = line.split(":", 3);
+            if(part.length != 3) continue;
+            Score score = new Score(part[0], Integer.parseInt(part[1]), part[2]);
+            scores.push(score);
             System.out.println(line);
         }
+        sort();
     }
 
-    public static void main(String[]args){
-        Highscore h = new Highscore("C:\\Users\\mamazu\\Desktop\\Tetris\\.gitignore");
+    private void sort(){
+        scores.sort((o1, o2) -> o2.compareTo(o1));
+    }
 
+    public void addScore(String username, int score){
+        Score newScore = new Score(username, score);
+        scores.push(newScore);
+        f.appendContent(newScore.toString());
+        sort();
+    }
+
+    public Score[] getTop(int amount){
+        if(amount < 1){return new Score[0];}
+        Score[] result = new Score[amount];
+        for (int i = 0; i < amount; i++) {
+            result[i] = scores.get(i);
+        }
+        return result;
+    }
+
+    public Score[] getAll(){ return getTop(scores.size()); }
+
+    public void save(){f.writeToDisk();}
+
+    public static void main(String[]args){
+        Highscore h = new Highscore("C:\\Users\\mamazu\\Desktop\\Tetris\\test.hs");
+        h.save();
+        System.out.print(Arrays.toString(h.getAll()));
     }
 }
