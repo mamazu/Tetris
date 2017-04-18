af390d126d52bc0e978ac5f2bbf73a5f30218e5b
mamazu <maxo.pech@gmail.com>
2017-04-19 01:04:54 +0200

    Added a score class

---
+++ src/Score/Score.java
@@ -0,0 +1,54 @@
+package Score;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Score {
+    private String username;
+    private int score;
+    private Date date;
+    private SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH-mm-ss");
+
+    public Score(String username, int score, String date) {
+        this(username,score);
+        setDate(date);
+    }
+
+    public Score(String username, int score) {
+        this.username = username;
+        this.score = score;
+        date = new Date();
+    }
+
+    public void setDate(String date) {
+        try {
+            this.date = sdf.parse(date);
+        } catch (ParseException e) {
+            System.err.println("Please use the iso-format YYYY-MM-dd");
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int compareTo(Score o2) {
+        if(score > o2.score) return 1;
+        if(score < o2.score) return -1;
+        return date.compareTo(o2.date);
+    }
+
+    @Override
+    public String toString() {
+        return username + ":" + score + ":" + sdf.format(date);
+    }
+}
