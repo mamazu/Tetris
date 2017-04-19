ff0527c9b961c569093ba833879b146469b56f3f
mamazu <maxo.pech@gmail.com>
2017-04-19 19:02:11 +0200

    Reformatted file and added position to stone

--- src/Stone.java
+++ src/Stone.java
@@ -1,38 +1,47 @@
-public class Stone{
+import java.awt.*;
 
-	StoneTypes type;
-	private boolean[][] pattern;
+public class Stone {
 
-	public Stone(){
-		this(StoneType.random());
-	}
+    StoneTypes type;
+    private boolean[][] pattern;
+    private Point position = null;
 
-	public Stone(StoneTypes type){
-		this.type = type;
-		pattern = StoneType.getPattern(type);
-	}
+    public Stone() {
+        this(StoneType.random());
+    }
 
-	public void rotate(){
-		boolean[][] newpattern = new boolean[4][4];
-		for (int x = 0; x < pattern.length; x++) {
-			for(int y = 0; y < pattern[x].length; y++)
+    public Stone(StoneTypes type) {
+        this.type = type;
+        pattern = StoneType.getPattern(type);
+    }
+
+    public Stone(StoneTypes type, Point position) {
+        this.type = type;
+        pattern = StoneType.getPattern(type);
+        this.position = position;
+    }
+
+    public void rotate() {
+        boolean[][] newpattern = new boolean[4][4];
+        for (int x = 0; x < pattern.length; x++) {
+            for (int y = 0; y < pattern[x].length; y++)
                 newpattern[x][3 - y] = pattern[y][x];
         }
-		pattern = newpattern;
-	}
+        pattern = newpattern;
+    }
 
-	public String getPattern(){
-		StringBuilder s = new StringBuilder();
-		for (boolean[] row: pattern) {
-			for(boolean field : row)
-				s.append(field ? "x" : "_");
-			s.append("\n");
-		}
-		return s.toString();
-	}
+    public String getPattern() {
+        StringBuilder s = new StringBuilder();
+        for (boolean[] row : pattern) {
+            for (boolean field : row)
+                s.append(field ? "x" : "_");
+            s.append("\n");
+        }
+        return s.toString();
+    }
 
-	@Override
-	public String toString() {
-		return StoneType.toString(type);
-	}
+    @Override
+    public String toString() {
+        return StoneType.toString(type);
+    }
 }
