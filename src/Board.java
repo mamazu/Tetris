dad6479810068e9098631208aeb0bc76415c5750
mamazu <maxo.pech@gmail.com>
2017-04-19 19:01:26 +0200

    Reformatted file and made getValue and setValue private

--- src/Board.java
+++ src/Board.java
@@ -6,43 +6,37 @@ class Board {
 
     private final boolean[] board;
 
-    public Board(){
+    public Board() {
         this(WIDTH, HEIGHT);
     }
 
-    public Board(int width, int height){
-        if(width > 0 && height > 0){
+    public Board(int width, int height) {
+        if (width > 0 && height > 0) {
             board = new boolean[height * width];
             this.width = width;
-        }else{
+        } else {
             board = new boolean[HEIGHT * WIDTH];
             this.width = WIDTH;
         }
     }
 
-    public boolean getValue(int x, int y){
-        int index = index(x, y);
-        if(index >= board.length || index < 0)
-            throw new IndexOutOfBoundsException("Can't access this index");
-        return board[index];
+    private boolean getValue(int x, int y) {
+        return board[index(x, y)];
     }
 
-    public void setValue(int x, int y, boolean value){
-        int index = index(x, y);
-        if(index >= board.length || index < 0)
-            throw new IndexOutOfBoundsException("Can't access this index");
-        board[index] = value;
+    private void setValue(int x, int y, boolean value) {
+        board[index(x, y)] = value;
     }
 
     private int index(int x, int y) {
-        return y*width + x;
+        return y * width + x;
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder result = new StringBuilder();
         result.append("\n+-------------------+");
-        for(int i = 0; i < board.length; i++){
-            if(i % width == 0)
+        for (int i = 0; i < board.length; i++) {
+            if (i % width == 0)
                 result.append("\n|");
             char filled = board[i] ? 'x' : ' ';
             result.append(filled).append("|");
