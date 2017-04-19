8927dcf9a41ec1c5aea1028e435ae2c002bb3159
mamazu <maxo.pech@gmail.com>
2017-04-19 18:59:47 +0200

    Implemented next stone getter and "iterator"

--- src/Game.java
+++ src/Game.java
@@ -6,10 +6,20 @@ public class Game{
 		Game g = new Game();
 		if(args.length == 1 && args[0].equals("cli")){
 			System.out.print(g.board);
-			System.out.println("Next stone is: " + g.next);
-			System.out.println("Next pattern is: \n" + g.next.getPattern());
-			return;
-		}
-		// Game with GUI
+            System.out.println("Next stone is: " + g.getNext());
+            System.out.println("Next pattern is: \n" + g.getNext().getPattern());
+            return;
+        }
+        // Game with GUI
 	}
+
+    public Stone next() {
+        Stone next = this.next;
+        this.next = new Stone();
+        return next;
+    }
+
+    public Stone getNext() {
+        return next;
+    }
 }
