6f94f38e300ef5d1f3d5311b94f3fa41e8558138
mamazu <maxo.pech@gmail.com>
2017-04-19 22:14:03 +0200

    Moved and reformated Scores and Highscore file

--- src/Score/Highscore.java
+++ src/Score/Highscore.java
@@ -1,7 +1,8 @@
+package Score;
+
 import Score.Score;
 import tools.FileHandler;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Highscore {
@@ -9,16 +10,16 @@ public class Highscore {
     public FileHandler f;
     private LinkedList<Score> scores = new LinkedList<Score>();
 
-    public Highscore(String name){
+    public Highscore(String name) {
         f = new FileHandler(name);
         load();
     }
 
     private void load() {
-        for(String line : f.getContent()){
-            if(line.startsWith("#")) continue;
+        for (String line : f.getContent()) {
+            if (line.startsWith("#")) continue;
             String[] part = line.split(":", 3);
-            if(part.length != 3) continue;
+            if (part.length != 3) continue;
             Score score = new Score(part[0], Integer.parseInt(part[1]), part[2]);
             scores.push(score);
             System.out.println(line);
@@ -26,28 +27,34 @@ public class Highscore {
         sort();
     }
 
-    private void sort(){
+    private void sort() {
         scores.sort((o1, o2) -> o2.compareTo(o1));
     }
 
-    public void addScore(String username, int score){
+    public void addScore(String username, int score) {
         Score newScore = new Score(username, score);
         scores.push(newScore);
         f.appendContent(newScore.toString());
         sort();
     }
 
-    public Score[] getTop(int amount){
-        if(amount < 1){return new Score[0];}
+    public Score[] getTop(int amount) {
+        if (amount < 1) {
+            return new Score[0];
+        }
         Score[] result = new Score[amount];
-        for (int i = 0; i < amount; i++) {
+        for (int i = 0; i < amount && i < scores.size(); i++) {
             result[i] = scores.get(i);
         }
         return result;
     }
 
-    public Score[] getAll(){ return getTop(scores.size()); }
+    public Score[] getAll() {
+        return getTop(scores.size());
+    }
 
-    public void save(){f.writeToDisk();}
+    public void save() {
+        f.writeToDisk();
+    }
 
 }
