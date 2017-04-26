2087dbc1b5cc95dec48275df0955fb413e04dc2f
mamazu <maxo.pech@gmail.com>
2017-04-26 17:52:09 +0200

    Fixed the rotated blocks

--- src/tools/StoneType.java
+++ src/tools/StoneType.java
@@ -16,22 +16,22 @@ public class StoneType {
         switch (x) {
             case BAR:
                 for (int i = 0; i < 4; i++)
-                    pattern.set(i, 0, true);
+                    pattern.set(0, i, true);
                 break;
             case J:
             case L:
                 for (int i = 0; i < 3; i++)
-                    pattern.set(0, i, true);
+                    pattern.set(i, 0, true);
                 if (x == StoneTypes.L) {
-                    pattern.set(1, 0, true);
+                    pattern.set(0, 1, true);
                 } else {
-                    pattern.set(1, 2, true);
+                    pattern.set(2, 1, true);
                 }
                 break;
             case T:
+                pattern.set(1, 0, true);
                 for (int i = 0; i < 3; i++)
-                    pattern.set(1, i, true);
-                pattern.set(0, 1, true);
+                    pattern.set(i, 1, true);
                 break;
             case SQUARE:
                 for (int i = 0; i <= 1; i++) {
@@ -39,17 +39,13 @@ public class StoneType {
                     pattern.set(1, i, true);
                 }
                 break;
+            case Z:
             case S:
                 for (int i = 0; i < 2; i++) {
-                    pattern.set(0, i + 1, true);
-                    pattern.set(1, i, true);
+                    pattern.set(i, (StoneTypes.S == x) ? 1 : 0, true);
+                    pattern.set(i+1, (StoneTypes.S == x) ? 0 : 1, true);
                 }
                 break;
-            case Z:
-                for (int i = 0; i < 2; i++) {
-                    pattern.set(0, i, true);
-                    pattern.set(1, i + 1, true);
-                }
         }
         return pattern;
     }
