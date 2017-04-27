018ab1c19ef6913e2ac6737f5c9b29a85da7251d
mamazu <maxo.pech@gmail.com>
2017-04-27 18:32:59 +0200

    Make the pattern closer fit the stone

--- src/tools/StoneType.java
+++ src/tools/StoneType.java
@@ -1,7 +1,6 @@
 package tools;
 
 import java.util.Random;
-import tools.StoneTypes;
 
 public class StoneType {
 
@@ -12,14 +11,15 @@ public class StoneType {
     }
 
     public static Pattern getPattern(StoneTypes x) {
-        Pattern pattern = new Pattern(4, 4);
+        Pattern pattern = new Pattern(1, 1);
         switch (x) {
             case BAR:
-                for (int i = 0; i < 4; i++)
-                    pattern.set(0, i, true);
-                break;
+                pattern= new Pattern(1, 4);
+                pattern.setAll(true);
+                return pattern;
             case J:
             case L:
+                pattern = new Pattern(2, 3);
                 for (int i = 0; i < 3; i++)
                     pattern.set(i, 0, true);
                 if (x == StoneTypes.L) {
@@ -29,18 +29,18 @@ public class StoneType {
                 }
                 break;
             case T:
+                pattern = new Pattern(3, 2);
                 pattern.set(1, 0, true);
                 for (int i = 0; i < 3; i++)
                     pattern.set(i, 1, true);
                 break;
             case SQUARE:
-                for (int i = 0; i <= 1; i++) {
-                    pattern.set(0, i, true);
-                    pattern.set(1, i, true);
-                }
-                break;
+                pattern = new Pattern(2, 2);
+                pattern.setAll(true);
+                return pattern;
             case Z:
             case S:
+                pattern = new Pattern(3, 2);
                 for (int i = 0; i < 2; i++) {
                     pattern.set(i, (StoneTypes.S == x) ? 1 : 0, true);
                     pattern.set(i+1, (StoneTypes.S == x) ? 0 : 1, true);
