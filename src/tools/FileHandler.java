6b74f62eaeb6173a9c3f620cb37497014beb3815
mamazu <maxo.pech@gmail.com>
2017-04-27 14:01:17 +0200

    Added message for new files and toString method

--- src/tools/FileHandler.java
+++ src/tools/FileHandler.java
@@ -1,9 +1,6 @@
 package tools;
 
 import java.io.*;
-import java.nio.file.AccessDeniedException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,12 +15,17 @@ public class FileHandler {
 
     private void load() {
         File f = new File(FILENAME);
-        if (!f.isFile())
+        if (!f.isFile()) {
+            System.out.print("Created new file");
             writeToDisk();
+        }
         Scanner scanner;
         try {
             scanner = new Scanner(new FileReader(f));
         } catch (FileNotFoundException e) {
+            System.err.print("Could not open file");
+            return;
+        }catch (Exception e){
             return;
         }
         while (scanner.hasNext()) {
@@ -43,17 +45,20 @@ public class FileHandler {
         return content.size();
     }
 
-    public boolean writeToDisk() {
-        StringBuilder string = new StringBuilder();
-        for (String s : content) string.append(s).append("\n");
+    public void writeToDisk() {
         try {
             BufferedWriter fw = new BufferedWriter(new FileWriter(FILENAME));
-            fw.write(string.toString());
+            fw.write(toString());
             fw.close();
         } catch (IOException e) {
             System.err.println("IO Exception");
-            return false;
         }
-        return true;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder string = new StringBuilder();
+        content.forEach((String s)-> string.append(s).append("\n"));
+        return string.toString();
     }
 }
